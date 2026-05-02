931e692c3588f7641dab861eb836b35c9c1f8dce
Sars <sarvelizalciva2006@gmail.com>
2026-05-02 17:13:46 -0500

    Fix route handling and define PORT variable

--- servidor.js
+++ servidor.js
@@ -12,10 +12,11 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/', listaRuta);
 
-app.get('/*path', (req, res) => {
+app.get('(.*)', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html')); 
 }); 
 
+const PORT =  process.env.PORT || 3000; 
 app.listen(PORT, () => {
   console.log("Se pudo unir en ${PORT}");
 });
